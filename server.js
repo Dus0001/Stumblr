0b1e9daa350d1ccb6551d52d3f8781bc96a9bc77
Daven <davensteen@gmail.com>
2022-12-12 01:49:02 -0600

    establish db and connection

---
+++ server.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const app = express();
+const sequelize = require('./config/connection'); 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'stumblr',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+const PORT = process.env.PORT || 3002;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log(`Now listening`))
+});
