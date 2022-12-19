fbbdde8b1a587c76c7def9d94ed956ac3c31e837
Daven <davensteen@gmail.com>
2022-12-18 20:45:19 -0600

    finish routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
 const commentRoutes= require('./comment-routes');
+const postRoutes = require('./post-routes');
+const usertRoutes = require('./user-routes')
 
 router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', usertRoutes);
+
+module.exports = router;
